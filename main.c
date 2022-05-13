a7ab8e1cfb11feabeaba3e86cf7b2c571abcec08
Thaïs <104143788+Nyxiamin@users.noreply.github.com>
2022-05-13 10:50:23 +0200

    Menu ajouté

--- main.c
+++ main.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
 #include "takuzu.h"
 
-int main() {
+void play(){
     int tl = choose();
     srand(time(NULL));
     int Mat[LENGTH][LENGTH];
     create_matrix(Mat, tl);
     int Masque[LENGTH][LENGTH];
-    print_full_matrix(Mat,tl);
-    printf("\nFiltre\n");
     Masque_matrix(Masque, tl);
-    print_full_matrix(Masque, tl);
-    printf("\nMatrice avec filtre\n");
     print_with_mask(Mat,Masque,tl);
+}
+void rules(){
+    printf("\n===These are the rules:===\n");
+    printf("1) There must be the same number of ZERO and ONE in a line or a column\n");
+    printf("2) Do not put 3 ONE or ZERO next to each other in a row or column\n");
+    printf("3) Lines and columns are all different\n");
+}
 
+int main() {
+    printf("\n      === TAKUZU MENU ===\n");
+    printf("1) Play the game\n");
+    printf("2) See rules\n");
+    int response = ask_menu();
+    switch (response){
+        case 1:
+            play();
+            break;
+        case 2:
+            rules();
+            int back;
+            printf("Enter 0 to return to the menu:");
+            scanf("%d", &back);
+            if (back==0) {
+                main();
+            }
+            break;
+        default:
+            printf("~Wrong response.~\n");
+            main();
+            break;
+    }
     return 0;
 }
